04804e28acd89acf6174d259ec6f49efb42dd94e
FRED-LAPTOP\Frederico <frederico.94@hotmail.com>
2019-06-05 18:28:31 +0100

    RecombinationCycleCrossover (ainda não implementado)

--- src/ga/geneticOperators/Recombination3.java
+++ src/ga/geneticOperators/Recombination3.java
@@ -13,7 +13,37 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
 
     @Override
     public void recombine(I ind1, I ind2) {
-        //TODO
+
+        int a = 3;
+        int cycleValue = a;
+        int cycleNumber = 1;
+
+        int[] child1 = new int[ind1.getNumGenes()];
+
+        int genesPreenchidos = 0;
+
+        while (genesPreenchidos != ind1.getNumGenes()) {
+
+            for (int j = 0; j < ind1.getNumGenes(); j++) {
+                if (ind1.getGene(j) == a){
+                    if (cycleNumber%2 == 0){
+
+                    }else{
+                        child1[j] = a;
+                        a = ind2.getGene(j);
+                        genesPreenchidos++;
+                        ind1.setGene(j,-ind1.getGene(j));
+
+                        if(cycleValue == a){
+                            cycleNumber++;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
     }
 
     @Override
