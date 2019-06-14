cf48f65ffc0be2c2100d569e285f7a269d9af9fc
FRED-LAPTOP\Frederico <frederico.94@hotmail.com>
2019-06-14 19:47:23 +0100

    Recombination Method Cycle Crossover

--- src/ga/geneticOperators/Recombination3.java
+++ src/ga/geneticOperators/Recombination3.java
@@ -6,6 +6,7 @@ import ga.Problem;
 public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>> extends Recombination<I, P> {
 
     //TODO this class might require the definition of additional methods and/or attributes
+    int[] child1, child2;
 
     public Recombination3(double probability) {
         super(probability);
@@ -13,42 +14,96 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
 
     @Override
     public void recombine(I ind1, I ind2) {
+        child1 = new int[ind1.getNumGenes()];
+        child2 = new int[ind1.getNumGenes()];
 
-        int a = 3;
-        int cycleValue = a;
+        cycle(ind1, ind2, child1);
+        vetorPositivo(ind1);
+        vetorPositivo(ind2);
+        cycle(ind2, ind1, child2);
+
+        System.arraycopy(child1, 0, ind1.getGenome(), 0, child1.length);
+        System.arraycopy(child2, 0, ind2.getGenome(), 0, child2.length);
+
+        return;
+    }
+
+    private void cycle(I ind1, I ind2, int[] child) {
+
+        int parentValue = ind1.getGene(0);
         int cycleNumber = 1;
 
-        int[] child1 = new int[ind1.getNumGenes()];
+        int[] childAux = new int[ind1.getNumGenes()];
 
         int genesPreenchidos = 0;
 
         while (genesPreenchidos != ind1.getNumGenes()) {
 
-            for (int j = 0; j < ind1.getNumGenes(); j++) {
-                if (ind1.getGene(j) == a){
-                    if (cycleNumber%2 == 0){
+            if (cycleNumber % 2 == 0) {
 
-                    }else{
-                        child1[j] = a;
-                        a = ind2.getGene(j);
-                        genesPreenchidos++;
-                        ind1.setGene(j,-ind1.getGene(j));
+                child[ind2.getIndexof(parentValue)] = parentValue;
+                childAux[parentValue - 1] = parentValue;
 
-                        if(cycleValue == a){
-                            cycleNumber++;
+                genesPreenchidos++;
+
+                int indexParentValue = ind2.getIndexof(parentValue);
+
+                ind1.setGene(indexParentValue, -ind1.getGene(indexParentValue));
+                ind2.setGene(indexParentValue, -ind2.getGene(indexParentValue));
+
+                parentValue = -ind1.getGene(indexParentValue);
+
+                if (childAux[parentValue - 1] == parentValue) {
+                    cycleNumber++;
+
+                    for (int i = 0; i < ind1.getNumGenes(); i++) {
+                        if (ind1.getGene(i) > 0){
+                            parentValue = ind1.getGene(i);
+                            break;
                         }
                     }
 
-                    break;
+                }
+
+            } else {
+
+                child[ind1.getIndexof(parentValue)] = parentValue;
+                childAux[parentValue - 1] = parentValue;
+
+                genesPreenchidos++;
+
+                int indexParentValue = ind1.getIndexof(parentValue);
+
+                ind1.setGene(indexParentValue, -ind1.getGene(indexParentValue));
+                ind2.setGene(indexParentValue, -ind2.getGene(indexParentValue));
+
+                parentValue = -ind2.getGene(indexParentValue);
+
+                if (childAux[parentValue - 1] == parentValue) {
+                    cycleNumber++;
+
+                    for (int i = 0; i < ind2.getNumGenes(); i++) {
+                        if (ind2.getGene(i) > 0){
+                            parentValue = ind2.getGene(i);
+                            break;
+                        }
+                    }
+
                 }
             }
-        }
 
+        }
+    }
+
+    private void vetorPositivo (I ind){
+        for (int i = 0; i < ind.getNumGenes(); i++) {
+            ind.setGene(i,Math.abs(ind.getGene(i)));
+        }
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         //TODO
         throw new UnsupportedOperationException();
-    }    
+    }
 }
